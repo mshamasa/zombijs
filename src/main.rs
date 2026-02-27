84300c704dae94e466d736411bcee0dcc25df331
Mike Shamasa <mshamasa@gmail.com>
2026-02-26 21:00:45 -0800

    feat: working state

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,53 @@ use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
 
+fn get_active_files(queue: &HashSet<String>) -> HashSet<String> {
+    let mut active_files_set: HashSet<String> = HashSet::new();
+
+    for key in queue.iter() {
+        let source_text = fs::read_to_string(key).unwrap_or_default();
+        active_files_set.insert(String::from(key));
+
+        let allocator = Allocator::default();
+        let source_type = SourceType::from_path(key).unwrap();
+
+        let ret = Parser::new(&allocator, &source_text, source_type)
+            .with_options(ParseOptions::default())
+            .parse();
+        let dir = Path::new(&key)
+            .parent()
+            .unwrap()
+            .to_str()
+            .unwrap_or_default();
+        for stmt in &ret.program.body {
+            if let ImportDeclaration(import) = stmt {
+                let source = import.source.to_string();
+                if source.starts_with(".") {
+                    let path = Path::new(&source);
+                    match path.extension() {
+                        Some(ext)
+                            if !["ts", "tsx", "js", "jsx"].contains(&ext.to_str().unwrap()) =>
+                        {
+                            continue;
+                        }
+                        None => {
+                            add_ext(dir, path, &mut active_files_set);
+                        }
+                        _ => {
+                            let x = Path::new(&dir)
+                                .join(&source)
+                                .canonicalize()
+                                .unwrap_or_default();
+                            active_files_set.insert(String::from(x.to_str().unwrap()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    active_files_set
+}
+
 fn add_ext(dir: &str, p: &Path, set: &mut HashSet<String>) {
     for ext in ["index.ts", "index.tsx"] {
         let temp = Path::new(dir)
@@ -32,8 +79,6 @@ fn setup_collections(dir: &str) -> (HashSet<String>, HashSet<String>) {
         let entry = entry.unwrap();
         let path = entry.path();
         // skip directories and all files that are NOT (JS|TS|JSX|TSX)
-        println!("walking=====dir={:?}, path={:?}", entry, path);
-        println!("extension====={:?}", path.extension());
         match path.extension() {
             None => continue,
             Some(e) => match e.to_str() {
@@ -65,13 +110,8 @@ fn setup_collections(dir: &str) -> (HashSet<String>, HashSet<String>) {
         for stmt in &ret.program.body {
             if let ImportDeclaration(import) = stmt {
                 let source = import.source.to_string();
-                println!("Import from: {}", source);
-                // todo - imports need to be full paths so dir + source
-                // todo - check path against common shorthand imports
-                // ./components/Card === ./components/Card/index.(ts|tsx)
                 if source.starts_with(".") {
                     let path = Path::new(&source);
-                    println!("path====={:?}", path.extension());
                     match path.extension() {
                         Some(ext)
                             if !["ts", "tsx", "js", "jsx"].contains(&ext.to_str().unwrap()) =>
@@ -102,16 +142,18 @@ fn setup_collections(dir: &str) -> (HashSet<String>, HashSet<String>) {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = Arguments::from_env();
-    println!("args====: {:?}", args);
     let dir: String = args
         .free_from_str()
         .expect("The first argument is required. Example: cargo run -- <valid directory>");
-    println!("Working dir====: {}", dir);
 
     let (all_files_set, imports_queue_set) = setup_collections(&dir);
+    let active_files_set = get_active_files(&imports_queue_set);
+    let dead_files = all_files_set.difference(&active_files_set);
 
-    println!("all files====={:?}", all_files_set);
-    println!("queue====={:?}", imports_queue_set);
+    println!("all_files_set====={:?}", all_files_set);
+    println!("imports_queue_set====={:?}", imports_queue_set);
+    println!("active_files_set====={:?}", active_files_set);
+    println!("dead_files====={:?}", dead_files);
 
     Ok(())
 }
